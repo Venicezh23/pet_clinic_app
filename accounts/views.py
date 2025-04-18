5de90e07edc13938333d434646f21720dd898e42
Venicezh23 <83906230+Venicezh23@users.noreply.github.com>
2025-04-18 21:13:03 +0800

    Register basic design

--- accounts/views.py
+++ accounts/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import PetOwner
+from .forms import CustomUserCreationForm
 
 # Register View
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            PetOwner.objects.create(user=user, name=user.username)  # Auto-create PetOwner
-            login(request, user)  # Log in the new user
-            return redirect("login")  # Redirect to the main page
+            PetOwner.objects.create(user=user, name=user.username)
+            login(request, user)
+            return redirect("login")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     
     return render(request, "accounts/register.html", {"form": form})
 
