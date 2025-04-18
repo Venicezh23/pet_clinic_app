5b918fa37456ce482a9632f522f07f6a9da0fb2d
Venicezh23 <83906230+Venicezh23@users.noreply.github.com>
2025-04-19 04:02:04 +0800

    Appointments Design
    Apoointments table design adjusted.
    Logic in views updated

--- appointments/views.py
+++ appointments/views.py
@@ -8,6 +8,9 @@ from accounts.models import PetOwner
 from django.http import JsonResponse, HttpResponse
 from django.utils import timezone
 from datetime import timedelta
+
+from django.utils.timezone import make_aware, get_current_timezone
+
 from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail
 
@@ -31,29 +34,28 @@ def book_appointment(request, pet_id):
 @login_required
 def appointment_list(request):
     try:
-        pet_owner = PetOwner.objects.get(user=request.user)  # Get the PetOwner instance
-        owner_pets = PetProfile.objects.filter(owner=pet_owner)  # Get all pets belonging to the owner
-        
-        selected_pet_id = request.GET.get('pet_id')  # Get pet_id from the dropdown
+        pet_owner = PetOwner.objects.get(user=request.user)
+        owner_pets = PetProfile.objects.filter(owner=pet_owner)
+        selected_pet_id = request.GET.get('pet_id')
 
-        if selected_pet_id:  # If a pet is selected, filter appointments by that pet
+        if selected_pet_id:
             appointments = Appointment.objects.filter(pet__id=selected_pet_id, pet__owner=pet_owner)
-        else:  # Otherwise, show all appointments for the pet owner
+        else:
             appointments = Appointment.objects.filter(pet__owner=pet_owner)
 
-        #check for appointments one day away from the current date
-        one_day_away = timezone.now().date() + timedelta(days=1)
-        upcoming_appointments = appointments.filter(appt_datetime__date=one_day_away)
+        # Appointments due within the next 24 hours
+        now = timezone.now()
+        next_24_hours = now + timedelta(hours=24)
+        upcoming_appointments = appointments.filter(appt_datetime__gte=now, appt_datetime__lte=next_24_hours)
 
     except PetOwner.DoesNotExist:
-        appointments = Appointment.objects.none()  # If the user has no pet, return empty
-        owner_pets = []  # No pets available
+        appointments = Appointment.objects.none()
+        owner_pets = []
         upcoming_appointments = []
 
     return render(request, 'appointments/appointment_list.html', {
         'appointments': appointments,
         'owner_pets': owner_pets,
         'selected_pet_id': selected_pet_id,
-        'upcoming_appointments':upcoming_appointments,
+        'upcoming_appointments': upcoming_appointments,
     })
-
