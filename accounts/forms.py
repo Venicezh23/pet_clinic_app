f446d651f046428625b73ddca9b7592728e48819
Venicezh23 <83906230+Venicezh23@users.noreply.github.com>
2025-04-20 22:30:49 +0800

    Register password validation improved
    Has error messages + checking

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
+    def clean_password1(self):
+        password = self.cleaned_data.get('password1')
+
+        if len(password) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        if len(re.findall(r"\d", password)) < 3:
+            raise ValidationError("Password must include at least 3 numbers.")
+        if not re.search(r"[^A-Za-z0-9]", password):
+            raise ValidationError("Password must include at least 1 special character.")
+
+        return password
