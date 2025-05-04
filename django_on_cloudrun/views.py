75c10a7c01c16a60b8717e2720470cd30309256f
Venicezh23 <83906230+Venicezh23@users.noreply.github.com>
2025-05-04 17:42:04 +0800

    Added some comments

--- django_on_cloudrun/views.py
+++ django_on_cloudrun/views.py
@@ -16,7 +16,7 @@ def generate_signed_url(blob_name):
     bucket = client.bucket(settings.GS_BUCKET_NAME)
     blob = bucket.blob(blob_name)
     url = blob.generate_signed_url(
-        expiration=timedelta(minutes=10),  # expires in 10 minutes
+        expiration=timedelta(minutes=10),  #expires in 10 minutes
         method='GET'
     )
     return url
@@ -25,9 +25,9 @@ def generate_signed_url(blob_name):
 def home(request):
     pets = []
     if request.user.is_authenticated:
-        try:
-            pet_owner = PetOwner.objects.get(user=request.user)  # Get PetOwner
-            pets = PetProfile.objects.filter(owner=pet_owner)  # Get pets
+        try: #if pet owner exists, get any pets they have
+            pet_owner = PetOwner.objects.get(user=request.user)
+            pets = PetProfile.objects.filter(owner=pet_owner)
 
             for pet in pets:
                 if pet.photo and pet.photo.name:
@@ -35,12 +35,12 @@ def home(request):
                 else:
                     pet.signed_url = None
         except PetOwner.DoesNotExist:
-            pets = []  # No PetOwner, return empty list
+            pets = []
 
     return render(request, "home.html", {"pets": pets})
 
 @login_required
-def pet_list(request):
+def pet_list(request): #display all pets an owner has
     try:
         pet_owner = PetOwner.objects.get(user=request.user)
         pets = PetProfile.objects.filter(owner=pet_owner)
@@ -56,20 +56,16 @@ def pet_list(request):
 
     return render(request, "pets/pet_list.html", {"pets": pets})
 
-
-def add_pet(request):
+def add_pet(request): #add pet and assign to current user
     if request.method == "POST":
         form = PetProfileForm(request.POST, request.FILES)
         if form.is_valid():
-            # Get the PetOwner associated with the logged-in user
             pet_owner = PetOwner.objects.get(user=request.user)
-
-            # Set the owner of the pet profile as the PetOwner instance
             pet_profile = form.save(commit=False)
-            pet_profile.owner = pet_owner  # Assign the PetOwner instance as the owner
-
+            pet_profile.owner = pet_owner
             pet_profile.save()
             return redirect('home')
     else:
         form = PetProfileForm()
     return render(request, "pets/add_pet.html", {"form": form})
+
